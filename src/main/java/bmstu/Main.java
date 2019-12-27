e87e24ad59912e196b7b0f73c9c2d65f6da5c6df
GromkovAlexander <gromkovalex@mail.ru>
2019-12-27 22:23:45 +0300

    Scripted auto-commit on change (2019-12-27 22:23:45) by gitwatch.sh

--- src/main/java/bmstu/Main.java
+++ src/main/java/bmstu/Main.java
@@ -2,8 +2,15 @@ package bmstu;
 import org.zeromq.ZContext;
 import org.zeromq.*;
 
+import java.net.Socket;
+
 public class Main {
     public static void main(String[] args) {
+
+        ZContext context = new ZContext();
+
+        ZMQ.Socket frontend = context.createSocket(SocketType.ROUTER);
+        ZMQ.Socket backend = context.createSocket(SocketType.ROUTER);
 
     }
 }
