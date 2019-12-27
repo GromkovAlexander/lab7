507d593b3a9f638d1e59aa5ae4311cf4c2cbd863
GromkovAlexander <gromkovalex@mail.ru>
2019-12-27 22:20:35 +0300

    Scripted auto-commit on change (2019-12-27 22:20:35) by gitwatch.sh

--- src/main/java/bmstu/Main.java
+++ src/main/java/bmstu/Main.java
@@ -1,7 +1,27 @@
 package bmstu;
+import org.zeromq.ZContext;
+import org.zeromq.*;
+
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("HELLO WORLD");
+        ZContext context = new ZContext();
+        ZMQ.Socket socket = context.createSocket(SocketType.REQ);
+        socket.bind("tcp://localhost:5559");
+        Scanner in = new Scanner(System.in);
+
+
+        while (true) {
+
+            String com = in.nextLine();
+
+            if (com.startsWith("GET")) {
+
+            } else if (com.startsWith("PUT")) {
+                
+            }
+
+        }
     }
 }
