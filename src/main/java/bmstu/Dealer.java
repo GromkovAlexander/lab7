580831de4e179271ef54044b60d7967bec162437
GromkovAlexander <gromkovalex@mail.ru>
2019-12-27 22:58:36 +0300

    Scripted auto-commit on change (2019-12-27 22:58:35) by gitwatch.sh

--- src/main/java/bmstu/Dealer.java
+++ src/main/java/bmstu/Dealer.java
@@ -15,6 +15,8 @@ public class Dealer {
     private final static String PUT = "PUT";
     private final static String GET = "GET";
 
+    private final static int TIME_DELAY = 10000;
+
     public static void main(String[] args) {
         ZContext context = new ZContext();
         ZMQ.Socket socket = context.createSocket(SocketType.REQ);
@@ -33,6 +35,13 @@ public class Dealer {
 
 
         while(!Thread.currentThread().isInterrupted()) {
+            items.poll(1);
+
+            if (System.currentTimeMillis() - time > TIME_DELAY) {
+                time = System.currentTimeMillis();
+                ZMsg msg = new ZMsg();
+
+            }
 
         }
 
