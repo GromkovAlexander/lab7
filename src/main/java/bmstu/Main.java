c1066e8e0a78c45812bbdef308de900eba39fd53
GromkovAlexander <gromkovalex@mail.ru>
2019-12-27 22:38:08 +0300

    Scripted auto-commit on change (2019-12-27 22:38:08) by gitwatch.sh

--- src/main/java/bmstu/Main.java
+++ src/main/java/bmstu/Main.java
@@ -1,10 +1,15 @@
 package bmstu;
+import javafx.util.Pair;
 import org.zeromq.ZContext;
 import org.zeromq.*;
 
 import java.net.Socket;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
+
+    private static HashMap<Pair<Integer, Integer>, Pair<ZFrame, Long>> data = new HashMap<>();
 
     private final static String SOCKET_FRONT = "tcp://localhost:5559";
     private final static String SOCKET_BACK = "tcp://localhost:5560";
@@ -32,22 +37,69 @@ public class Main {
             items.poll();
 
             if (items.pollin(0)) {
-                ZMsg messageFromFront = ZMsg.recvMsg(frontend);
-                more = frontend.hasReceiveMore();
+                while (true) {
+                    ZMsg messageFromFront = ZMsg.recvMsg(frontend);
 
-                ZFrame adress = messageFromFront.pop();
-                ZFrame nullFrame = messageFromFront.pop();
+                    ZFrame adress = messageFromFront.pop();
+                    ZFrame nullFrame = messageFromFront.pop();
 
-                String command = messageFromFront.popString();
+                    String command = messageFromFront.popString();
 
-                if (command.equals(PUT)) {
-                    ZFrame index = messageFromFront.pop();
-                } else if (command.equals(GET)) {
+                    if (command.equals(PUT)) {
+                        ZFrame index = messageFromFront.pop();
+                        int parseIndex = Integer.parseInt(index.toString());
+
+                        ZFrame characher = messageFromFront.pop();
+
+                        for (Map.Entry<Pair<Integer, Integer>, Pair<ZFrame, Long>> serverInfo : data.entrySet()) {
+                            int left = serverInfo.getKey().getKey();
+                            int right = serverInfo.getKey().getValue();
+                            if (left <= parseIndex && right > parseIndex) {
+                                ZFrame serverAdress = serverInfo.getValue().getKey().duplicate();
+                                sendMsg(backend, serverAdress, adress, index, characher);
+                            }
+                        }
+
+
+                    } else if (command.equals(GET)) {
+                        ZFrame index = messageFromFront.pop();
+                        int parseIndex = Integer.parseInt(index.toString());
+
+                        for (Map.Entry<Pair<Integer, Integer>, Pair<ZFrame, Long>> serverInfo : data.entrySet()) {
+                            int left = serverInfo.getKey().getKey();
+                            int right = serverInfo.getKey().getValue();
+                            if (left <= parseIndex && right > parseIndex) {
+                                ZFrame serverAdress = serverInfo.getValue().getKey().duplicate();
+                                sendMsg(backend, serverAdress, adress, index);
+                            }
+                        }
+                    }
+
+                    more = frontend.hasReceiveMore();
+
+                    if (!more) {
+                        break;
+                    }
 
                 }
+
+            }
+
+            if (items.pollin(1)) {
+
             }
 
         }
 
+    }
+
+
+
+    private static void sendMsg(ZMQ.Socket backend, ZFrame ... frames) {
+        ZMsg msgToBackend = new ZMsg();
+        for (ZFrame frame : frames) {
+            msgToBackend.add(frame);
+        }
+        msgToBackend.send(backend);
     }
 }
